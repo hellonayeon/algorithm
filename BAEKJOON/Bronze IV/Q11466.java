165ef20fbbc1b848d542d96afdd7fee8863c1df2
hellonayeon <nayeonkwon9815@gmail.com>
2021-05-28 23:56:52 +0900

    Bronze IV - 37/112

--- BAEKJOON/Bronze IV/Q11466.java
+++ BAEKJOON/Bronze IV/Q11466.java
@@ -5,15 +5,27 @@ class Q11466 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         br.close();
 
         float h = Float.parseFloat(st.nextToken());
         float w = Float.parseFloat(st.nextToken());
 
-        float res = (h > w) ? h/3 : h/2;
-        bw.write(Float.toString(res));
+        float tmp;
+        if (h < w) {
+            tmp = h;
+            h = w;
+            w = tmp;
+        }
+
+        if (h > w * 3)
+            bw.write(Float.toString(w));
+        else if (h > w * 1.5)
+            bw.write(Float.toString(h/3));
+        else
+            bw.write(Float.toString(w/2));
+
         bw.flush();
         bw.close();
     }
