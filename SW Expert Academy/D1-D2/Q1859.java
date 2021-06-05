4d68454c695f8d16f35bc265269fe639f7391772
hellonayeon <nayeonkwon9815@gmail.com>
2021-06-05 23:36:37 +0900

    1859. 백만 장자 프로젝트[알고리즘 수정]

--- SW Expert Academy/D1-D2/Q1859.java
+++ SW Expert Academy/D1-D2/Q1859.java
@@ -4,7 +4,6 @@ import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 
 import java.io.FileInputStream;
-import java.math.BigInteger;
 
 import java.util.StringTokenizer;
 
@@ -30,35 +29,20 @@ class Solution
             for(int idx = 0; idx < N; idx++) {
             	n[idx] = Integer.parseInt(st.nextToken());
             }
-            
-            int max = 0, mIdx = 0 ; // max 값을 갖고있는 index
-            int sIdx = 0; // 매입을 시작할 index
-            BigInteger profit = new BigInteger("0");
-            while(sIdx < n.length) {    
-                // 최댓값 구하는 루프          
-                for(int i = sIdx; i < n.length; i++) {
-                    if(n[i] >= max) {
-                        max = n[i];
-                        mIdx = i;
-                    }
-                }
 
-                // 매입, 매매 처리 루프 
-                for(int i = sIdx; i < mIdx; i++) {
-                    // profit -= n[i];
-                    profit = profit.subtract(new BigInteger(String.valueOf(n[i])));
-                }
-                // profit += n[mIdx] * (mIdx - sIdx);
-                
-                profit = profit.add(new BigInteger(String.valueOf( (new BigInteger(String.valueOf(n[mIdx]))).multiply(new BigInteger(String.valueOf(mIdx-sIdx))))));
-
-                sIdx = mIdx + 1;
-                max = 0;
+            long profit = 0;
+            int max = n[N-1];
+ 
+            for(int i = N-2; i >= 0; i--) {
+                if(n[i] > max) max = n[i];
+                else profit += max - n[i];
             }
+            
             sb.append("#").append(test_case).append(" ").append(profit).append("\n");
 		}
 		bw.write(sb.toString());
         bw.flush();
+        
         bw.close();
         br.close();
     }
