8a5514cb168ed6759861c9c89b3e29706586c215
hellonayeon <nayeonkwon9815@gmail.com>
2021-06-06 22:12:43 +0900

    2058. 자릿수 더하기[알고리즘 수정]

--- SW Expert Academy/D1-D2/AddDigits.java
+++ SW Expert Academy/D1-D2/AddDigits.java
@@ -14,13 +14,11 @@ class AddDigits {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+        String N = br.readLine();
 
         int sum = 0;
-        while(N > 0) {
-            sum += N % 10;
-            N /= 10;
-        }
+        for(int i = 0; i < N.length(); i++)
+            sum += N.charAt(i) - '0';
 
         bw.write(String.valueOf(sum));
         bw.flush();
