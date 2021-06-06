f3279e8dea4b81ea1425a68d1487bbdb744891a4
hellonayeon <nayeonkwon9815@gmail.com>
2021-06-06 22:04:10 +0900

    2056. 연월일 달력[알고리즘 수정]

--- SW Expert Academy/D1-D2/Calendar.java
+++ SW Expert Academy/D1-D2/Calendar.java
@@ -11,32 +11,29 @@ class Calendar {
     public static void main(String args[]) throws IOException {
         System.setIn(new FileInputStream("res/Calendar_input.txt"));
 
-        int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int[] days = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
         for (int test_case = 1; test_case <= T; test_case++) {
 
             String Input = br.readLine();
 
-            int month = Integer.parseInt(Input.substring(4, 6));
-            int date = Integer.parseInt(Input.substring(6, 8));
-
+            String year = Input.substring(0, 4);
+            String month = Input.substring(4, 6);
+            String date = Input.substring(6);
         
-            if( (month >= 1 && month <= 12) && (date != 0 && days[month-1] - date >= 0) ) {
-                sb.append("#").append(test_case).append(" ");
-                sb.append(Input.substring(0, 4)).append("/").append(Input.substring(4, 6)).append("/").append(Input.substring(6, 8)).append("\n");
+            if( Integer.parseInt(date) <= days[Integer.parseInt(month)] ) {
+                bw.write("#" + test_case + " " + year + "/" + month + "/" + date + "\n");
             }
             else {
-                sb.append("#").append(test_case).append(" ").append(-1).append("\n");
+                bw.write("#" + test_case + " -1" + "\n");
             }
         }
 
-        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
