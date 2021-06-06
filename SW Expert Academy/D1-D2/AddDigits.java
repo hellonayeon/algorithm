62672b14b7be7a3198b058c3cabe353eedd614e9
hellonayeon <nayeonkwon9815@gmail.com>
2021-06-06 22:10:37 +0900

    2058. 자릿수 더하기

---
+++ SW Expert Academy/D1-D2/AddDigits.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+import java.io.FileInputStream;
+
+// Q2058. 자릿수 더하기
+class AddDigits {
+    public static void main(String args[]) throws IOException {
+        System.setIn(new FileInputStream("res/AddDigits_input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        while(N > 0) {
+            sum += N % 10;
+            N /= 10;
+        }
+
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+    }
+}
