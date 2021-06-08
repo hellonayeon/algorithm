921e21794b1eb92aed96bfc698e0c08fdbca4ed8
hellonayeon <nayeonkwon9815@gmail.com>
2021-06-08 16:15:15 +0900

    [S/W 문제해결 기본] 1일차 - Flatten [알고리즘 수정]

--- SW Expert Academy/D2-D3/Flatten.java
+++ SW Expert Academy/D2-D3/Flatten.java
@@ -1,25 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.io.IOException;
 
 import java.io.FileInputStream;
 
 class Flatten {
+
+    static int[] box = new int[100];
+    static int dumpCount;
     public static void main(String args[]) throws IOException {
         System.setIn(new FileInputStream("res/Flatten_input.txt"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         for (int tc = 1; tc <= 10; tc++) {
-            int dumpCount = Integer.parseInt(br.readLine());
+            dumpCount = Integer.parseInt(br.readLine());
             String[] str = br.readLine().split(" ");
 
-            int[] box = new int[100];
             for(int idx = 0; idx < box.length; idx++)
                 box[idx] = Integer.parseInt(str[idx]);
 
-            insertSort(box);
-            int diff = dump(box, dumpCount);
+            Arrays.sort(box);
+            dump();
+
+            int diff = box[99] - box[0];
 
             System.out.println("#" + tc + " " + diff);
         }
@@ -27,40 +32,15 @@ class Flatten {
         br.close();
     }
 
-    public static int dump(int[] arr, int dumpCount) {
-        int high = arr.length-1;
-        int low = 0;
+    public static void dump() {
+        if(dumpCount == 0) return;
         
-        int diff = -1;
         while(dumpCount > 0) {
-            arr[high] -= 1;
-            arr[low] += 1;
-
+            box[0] += 1;
+            box[99] -= 1;
             dumpCount--;
-
-            insertSort(arr);
-            diff = arr[arr.length - 1] - arr[0];
-            if(diff == 0 || diff == 1) return diff;
-
-            if(arr[high] < arr[high - 1]) high -= 1;
-            if(arr[low] > arr[low + 1]) low += 1;
-        }
-
-        return diff;
-    }
-
-    public static void insertSort(int[] arr) {
-        for(int i = 1; i < arr.length; i++) {
-			int element = arr[i];
-			
-			int j = i - 1;
-			
-			while(j >= 0 && element < arr[j]) {
-				arr[j + 1] = arr[j];
-				j--;
-			}
-
-			arr[j + 1] = element;	
+        
+            Arrays.sort(box);
         }
     }
 }
