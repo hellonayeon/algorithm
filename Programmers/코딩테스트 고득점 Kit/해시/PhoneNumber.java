fede7f3cdccd3bffadeebac9adb1217cd8fe115b
hellonayeon <nayeonkwon9815@gmail.com>
2021-06-19 14:28:25 +0900

    전화번호 목록

--- Programmers/코딩테스트 고득점 Kit/해시/PhoneNumber.java
+++ Programmers/코딩테스트 고득점 Kit/해시/PhoneNumber.java
@@ -1,25 +1,12 @@
-import java.util.HashMap;
+import java.util.Arrays;
 
 class PhoneNumber {
-    public boolean solution(String[] phone_book) {
-        
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < phone_book.length; i++)
-            map.put(phone_book[i], phone_book[i].length());
-        
-        for(String prefix : map.keySet()) {
-            
-            int pre_len = map.get(prefix);
-            
-            for(String phone_number : map.keySet()) {
-                if(phone_number == prefix) continue;
-                
-                int num_len = map.get(phone_number);
-                if(num_len > pre_len && phone_number.substring(0, pre_len).equals(prefix))
-                    return false;
-                    
-            }
+    public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            String prefix = phone_book[i];
+            if(phone_book[i+1].startsWith(prefix)) return false;
         }
         return true;
     }
