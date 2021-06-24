7a697066a938971ed4213bb232fc3adaa98a62a2
hellonayeon <nayeonkwon9815@gmail.com>
2021-06-24 16:37:42 +0900

    네트워크[알고리즘 수정]

--- Programmers/코딩테스트 고득점 Kit/DFS.BFS/Network.java
+++ Programmers/코딩테스트 고득점 Kit/DFS.BFS/Network.java
@@ -1,22 +1,24 @@
 class Network {
     public int solution(int n, int[][] computers) {
-        boolean[] visit = new boolean[computers.length];
+        boolean[] visit = new boolean[n];
         int ans = 0;
 
-        for(int i = 0; i < computers.length; i++)
-            if(!visit[i]) ans += dfs(computers, visit, i);
+        // 모든 컴퓨터들을 검사
+        for(int i = 0; i < n; i++) {
+            if(!visit[i]) {
+                dfs(computers, visit, i);
+                ans++;
+            }
+        }   
 
         return ans;
     }
-
-    public int dfs(int[][] computers, boolean[] visit, int i) {        
-        if(visit[i]) return 0;
-
+    
+    // 하나의 컴퓨터에 연결된 다른 모든 컴퓨터들 검사
+    public void dfs(int[][] computers, boolean[] visit, int i) {        
         visit[i] = true;
-        for(int j = 0; j < computers.length; j++) {
-            if(i != j && computers[i][j] == 1) dfs(computers, visit, j);
-        }
-
-        return 1;
+        
+        for(int j = 0; j < computers.length; j++)
+            if(!visit[j] && i != j && computers[i][j] == 1) dfs(computers, visit, j);
     }
 }
