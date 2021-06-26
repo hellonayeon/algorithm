337e58456eb84788fb5d966e0f58630f745ad4f8
hellonayeon <nayeonkwon9815@gmail.com>
2021-06-26 12:46:52 +0900

    두 정수 사이의 합[알고리즘 수정]

--- Programmers/모든 문제/Level1/SumBetweenTwoIntegers.java
+++ Programmers/모든 문제/Level1/SumBetweenTwoIntegers.java
@@ -1,12 +1,8 @@
 class SumBetweenTwoIntegers {
     public long solution(int a, int b) {
-        long ans = 0;
-
-        int n = Math.min(a, b);
-        int range = Math.abs(a-b);
-        for(int i = 0; i <= range; i++)
-            ans += n++;   
-
-        return ans;
+        long n1 = Math.min(a, b);
+        long n2 = a + b - n1;
+        // 등차수열의 합
+        return (n2 - n1 + 1) * (n1 + n2) / 2;
     }
 }
