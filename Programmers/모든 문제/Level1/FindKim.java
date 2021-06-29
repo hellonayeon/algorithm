fd3ef85f9aa676866e386b219aa2187a8d638021
hellonayeon <nayeonkwon9815@gmail.com>
2021-06-29 23:53:11 +0900

    서울에서 김서방 찾기[알고리즘 수정]

--- Programmers/모든 문제/Level1/FindKim.java
+++ Programmers/모든 문제/Level1/FindKim.java
@@ -1,12 +1,8 @@
+import java.util.Arrays;
+
 class FindKim {
     public String solution(String[] seoul) {
-        int loc = 0;    
-        for(int i = 0; i < seoul.length; i++) {
-            if(seoul[i].equals("Kim")) {
-                loc = i;
-                break;
-            }
-        }
+        int loc = Arrays.asList(seoul).indexOf("Kim");
         return "김서방은 " + loc + "에 있다";
-    }    
+    }  
 }
