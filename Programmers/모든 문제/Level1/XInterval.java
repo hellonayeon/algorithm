7d3142d693ec1ddd7c2d7a39b8aa78067034ed5e
hellonayeon <nayeonkwon9815@gmail.com>
2021-07-06 18:04:34 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ Programmers/모든 문제/Level1/XInterval.java
@@ -0,0 +1,11 @@
+class XInterval {
+    public long[] solution(int x, int n) {
+        long[] ans = new long[n];
+        
+        ans[0] = x;
+        for(int i = 1; i < n; i++)
+            ans[i] = ans[i - 1] + x;
+        
+        return ans;
+    }   
+}
