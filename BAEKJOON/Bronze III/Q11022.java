e5090b27e75e1fadd3a1d050773fe9df5b534e29
hellonayeon <nayeonkwon9815@gmail.com>
2021-07-17 19:37:44 +0900

    A+B - 8

---
+++ BAEKJOON/Bronze III/Q11022.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Q11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+
+            sb.append("Case #").append(i).append(": ")
+              .append(n1).append(" + ").append(n2).append(" = ").append(n1+n2).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
