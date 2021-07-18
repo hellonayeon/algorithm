fd9f4874351b8b3913f675eeec37ba6840405fde
hellonayeon <nayeonkwon9815@gmail.com>
2021-07-18 15:02:22 +0900

    A+B - 4

---
+++ BAEKJOON/Bronze III/Q10951.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringBuilder sb = new StringBuilder();
+        String str;
+        while((str = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(str, " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append(A + B).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
