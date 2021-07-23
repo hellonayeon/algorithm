73dcde6f05bbe7b1ff07d4ef9c497e47dcd1e021
hellonayeon <nayeonkwon9815@gmail.com>
2021-07-23 16:22:49 +0900

    피보나치 수 5

---
+++ BAEKJOON/Bronze II/Q10870.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class Q10870 {
+    public static int fibonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        
+        return fibonacci(n - 1) + fibonacci(n  - 2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print(fibonacci(n));
+    }
+}
