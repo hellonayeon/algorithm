9dee3e5504cddedca67fab9eb7bbb48b02431fa8
hellonayeon <hellonayeon@icloud.com>
2021-08-18 13:56:01 +0900

    방금그곡[알고리즘 수정]

--- Programmers/모든 문제/Level2/ThatSong.java
+++ Programmers/모든 문제/Level2/ThatSong.java
@@ -1,84 +1,52 @@
-import java.util.ArrayList;
-
-class Music {
-    String title; // 음악 제목
-    int time; // 재생 시간
-    String melodies; // 재생 시간 동안 송출된 음
-    
-    public Music(String[] musicinfo) {
-        this.title = musicinfo[2];
-        calcTime(musicinfo[0], musicinfo[1]);
-        calcMelody(musicinfo[3]);
-    }
-    
-    public Music(String m) {
-        time = m.replaceAll("#","").length();
-        calcMelody(m);
-    }
-    
-    private void calcTime(String st, String et) {
-        String[] t1 = st.split(":");
-        String[] t2 = et.split(":");
-        
-        int h1 = Integer.parseInt(t1[0]);
-        int h2 = Integer.parseInt(t2[0]);
-        
-        int m1 = Integer.parseInt(t1[1]);
-        int m2 = Integer.parseInt(t2[1]);
-        
-        this.time = (h2 * 60 + m2) - (h1 * 60 + m1);
-    }
-    
-    private void calcMelody(String melody) {
-        int t = time;
-        int next = 0;
-        int len = melody.length();
-        
-        this.melodies = "";
-        
-        while(t > 0) {            
-            // 한 문자 뒤에 '#'이 있을 경우 체크
-            // '#'이 있는 경우 음을 소문자로 변경
-            if(next < len - 1 && melody.charAt(next + 1) == '#')
-                melodies += (char)(melody.charAt(next++) + 32);
-            else 
-                melodies += melody.charAt(next);
-            
-            next = (++next) % len;
-            --t;
-        }
-        
-    }
-}
-
 class ThatSong {
     public String solution(String m, String[] musicinfos) {
-        ArrayList<Music> list = new ArrayList<>();
-        ArrayList<Music> res = new ArrayList<>();
-        
-        // 네오가 기억하는 멜로디 정보를 담은 객체 생성
-        Music neo = new Music(m);
-        
-        // 방속된 곡의 정보를 담고있는 객체 생성
-        for(String music : musicinfos)
-            list.add(new Music(music.split(",")));    
-        
-        // 네오가 들은 곡 리스트 추출
-        for(Music music : list)
-            if(music.melodies.contains(neo.melodies)) res.add(music); 
-        
-        // 조건이 일치하는 음악이 없는 경우
-        if(res.size() == 0) return "(None)";
-        
-        Music ans = res.get(0);  
-        
-        if(res.size() > 1) {            
-            for(int i = 1; i < res.size(); i++) {
-                Music music = res.get(i);
-                if(music.time > ans.time) ans = music;
+        String answer = "(None)";
+        int time = 0;
+
+        m = edit(m);
+
+        for (int inx = 0; inx < musicinfos.length; inx++) {
+
+            String[] info = musicinfos[inx].split(",");
+
+            int start = (60 * Integer.parseInt(info[0].substring(0, 2)) + Integer.parseInt(info[0].substring(3)));
+            int end = (60 * Integer.parseInt(info[1].substring(0, 2)) + Integer.parseInt(info[1].substring(3)));
+            int t = end - start;
+
+            // 재생 시간이 이전 곡의 재생 시간보다 짧을 경우
+            // 네오가 들은 멜로디가 포함되어 있다 해도 답이 될 수 없다.
+            
+            // [문제 조건] 
+            // 조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환한다. 
+            // 재생된 시간도 같을 경우 먼저 입력된 음악 제목을 반환한다.
+
+            // 따라서 이전에 검사한 정답 후보들 보다
+            // 재생 시간이 짧은 경우에는 굳이 검사할 필요 없다. (t > time)
+            if (t > time) {
+                info[3] = edit(info[3]);
+                StringBuffer sb = new StringBuffer();
+                for (int jnx = 0; jnx < t; jnx++) {
+                    sb.append(info[3].charAt(jnx % (info[3].length())));
+                }
+                if (sb.toString().indexOf(m) >= 0) {
+                    answer = info[2];
+                    time = t;
+                }
             }
         }
-        
-        return ans.title;
+
+        return answer;
+    }
+
+    // 치환해야하는 문자열이 많은 것이 아니니까 이 방법도 좋은 해결책
+    public String edit(String m) {
+
+        m = m.replaceAll("C#", "V");
+        m = m.replaceAll("D#", "W");
+        m = m.replaceAll("F#", "X");
+        m = m.replaceAll("G#", "Y");
+        m = m.replaceAll("A#", "Z");
+
+        return m;
     }
 }
