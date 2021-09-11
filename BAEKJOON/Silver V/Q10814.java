b8647e8e7655fa36e384648eb35412fe237688ea
hellonayeon <hellonayeon@icloud.com>
2021-09-11 21:48:22 +0900

    나이순 정렬 [알고리즘 수정]

--- BAEKJOON/Silver V/Q10814.java
+++ BAEKJOON/Silver V/Q10814.java
@@ -1,48 +1,27 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
-import java.util.ArrayList;
-import java.util.Collections;
-
-class User implements Comparable<User>{
-    int age;
-    String name;
-    int joinOrder;
-
-    public User(String info, int joinOrder) {
-        String[] infos = info.split(" ");
-        this.age = Integer.parseInt(infos[0]);
-        this.name = infos[1];
-        this.joinOrder = joinOrder;
-    }
-
-    @Override
-    public int compareTo(User u) {
-        if(this.age == u.age) return this.joinOrder - u.joinOrder;
-
-        return this.age - u.age;
-    }
-}
+import java.util.StringTokenizer;
 
 class Q10814 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        StringBuilder[] ageSb = new StringBuilder[201];
+        for(int i = 0; i < 201; i++)
+            ageSb[i] = new StringBuilder();
 
-        ArrayList<User> userList = new ArrayList<>();
         for(int i = 0; i < N; i++) {
-            User user = new User(br.readLine(), i+1);
-            userList.add(user);
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int age = Integer.parseInt(st.nextToken());
+            ageSb[age].append(age).append(" ").append(st.nextToken()).append("\n");
         }
 
-        Collections.sort(userList);
-
-        StringBuilder sb = new StringBuilder();
-        for(User u : userList) {
-            sb.append(u.age).append(" ").append(u.name).append("\n");
+        StringBuilder ans = new StringBuilder();
+        for(StringBuilder sb : ageSb) {
+            ans.append(sb);
         }
 
-        System.out.print(sb);
+        System.out.println(ans);
     }
 }
