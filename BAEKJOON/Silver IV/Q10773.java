799c654415c0a80a4950311f124c565501feb3a2
hellonayeon <hellonayeon@icloud.com>
2021-10-22 18:25:04 +0900

    제로 [자료구조 수정]

--- BAEKJOON/Silver IV/Q10773.java
+++ BAEKJOON/Silver IV/Q10773.java
@@ -2,22 +2,22 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import java.util.Stack;
-
 class Q10773 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int K = Integer.parseInt(br.readLine());
+        
+        int[] stack = new int[K];
+        int size = 0;
 
-        Stack<Integer> stack = new Stack<>();
         while(K-- > 0) {
             int n = Integer.parseInt(br.readLine());
             if(n == 0) {
-                stack.pop();
+                stack[--size] = 0;
                 continue;
             }
 
-            stack.push(n);
+            stack[size++] = n;
         }
 
         int sum = 0;
