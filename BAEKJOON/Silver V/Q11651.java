3e335eba777247ce0f34947cdf05043513609830
hellonayeon <hellonayeon@icloud.com>
2021-10-22 12:13:25 +0900

    좌표 정렬하기 2 [알고리즘 수정]

--- BAEKJOON/Silver V/Q11651.java
+++ BAEKJOON/Silver V/Q11651.java
@@ -3,50 +3,33 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import java.util.StringTokenizer;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 class Q11651 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        ArrayList<Coordinate> xys = new ArrayList<>();
         
         int N = Integer.parseInt(br.readLine());
+        int[][] xys = new int[N][2];
         for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            xys.add(new Coordinate(x, y));
+            xys[i][0] = x;
+            xys[i][1] = y;
         }
 
-        Collections.sort(xys);
-        for(Coordinate c : xys) {
-            sb.append(c).append("\n");
+        Arrays.sort(xys, (e1, e2) -> {
+            if(e1[1] == e2[1]) return e1[0] - e2[0];
+            return e1[1] - e2[1];
+        });
+
+        for(int i = 0; i < N; i++) {
+            sb.append(xys[i][0]).append(" ").append(xys[i][1]).append("\n");
         }
 
         System.out.println(sb);
-    }
-}
-
-class Coordinate implements Comparable<Coordinate> {
-    int x;
-    int y;
-
-    public Coordinate(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public int compareTo(Coordinate c) {
-        if(this.y == c.y) return this.x - c.x;
-        return this.y - c.y;
-    }
-
-    @Override
-    public String toString() {
-        return this.x + " "  + this.y;
     }
 }
