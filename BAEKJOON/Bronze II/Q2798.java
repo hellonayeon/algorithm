d1f50acf919ccc093b0e6d5539c196d549d065e6
hellonayeon <hellonayeon@icloud.com>
2021-11-08 23:04:19 +0900

    블랙잭 [알고리즘 수정]

--- BAEKJOON/Bronze II/Q2798.java
+++ BAEKJOON/Bronze II/Q2798.java
@@ -4,44 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Q2798 {
-
-    static int N;
-    static int M;
-    static int[] arr;
-    static int max = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[N];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for(int idx = 0; idx < N; idx++) {
             arr[idx] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(new boolean[N], 0, 0);
-
-        System.out.println(max);
+        System.out.println(search(N, M, arr));
     }
 
-    public static void dfs(boolean[] chk, int sum, int depth) {
-        if(depth == 3) {
-            if(sum <= M) {
-                max = (sum > max) ? sum : max;
+    public static int search(int N, int M, int[] arr) {
+        int max = 0;
+
+        for(int i = 0; i < N-2; i++) {
+            for(int j = i+1; j < N-1; j++) {
+                for(int k = j+1; k < N; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+
+                    if(sum == M) return M;
+                    if(sum < M) {
+                        max = (sum > max) ? sum : max;
+                    }
+                }
             }
-            return;
         }
-        
-        for(int i = 0; i < N; i++) {
-            if(!chk[i]) {
-                chk[i] = true;
-                dfs(chk, sum + arr[i], depth+1);
-                chk[i] = false;
-            }
-        }
+
+        return max;
     }
 }
