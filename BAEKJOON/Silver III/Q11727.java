a30f4d507679b7fe61de72d3616ea93803e3f5aa
hellonayeon <hellonykwon@gmail.com>
2021-12-14 16:26:37 +0900

    2xn 타일링 2

--- BAEKJOON/Silver III/Q11727.java
+++ BAEKJOON/Silver III/Q11727.java
@@ -2,19 +2,18 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-class Q11726 {
+class Q11727 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        if(n != 1) dp[2] = 3;
 
-        int len = n+2;
-        int[] arr = new int[len];
-        arr[1] = 1;
-
-        for(int i = 2; i < len; i++) {
-            arr[i] = (arr[i-1] + arr[i-2]) % 10007;
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007;
         }
 
-        System.out.println(arr[len-1]);
+        System.out.println(dp[n]);
     }
 }
