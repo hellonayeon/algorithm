d64dab9c7f8a5ff2063b48ccea15f1d100247318
hellonayeon <hellonykwon@gmail.com>
2021-12-22 00:00:26 +0900

    이친수

---
+++ BAEKJOON/Silver III/Q2193.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Q2193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N+1];
+        dp[1] = 1;
+        if(N != 1) dp[2] = 1;
+
+        for(int i=3; i<=N; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(dp[N]);
+    }
+}
