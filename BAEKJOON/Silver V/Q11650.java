5e2e5dd2b5a36bb9b61f8da70b06a4fdcfdce111
hellonayeon <hellonykwon@gmail.com>
2021-12-22 18:21:26 +0900

    좌표 정렬하기 [알고리즘 수정]

--- BAEKJOON/Silver V/Q11650.java
+++ BAEKJOON/Silver V/Q11650.java
@@ -1,29 +1,43 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 class Q11650 {
+
+    static class Point implements Comparable<Point> {
+
+        int x;
+        int y;
+
+        public Point(int x,  int y) {
+            this.x = x; 
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return (this.x != o.x) ? this.x - o.x : this.y - o.y;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[][] arr = new int[N][2];
+        PriorityQueue<Point> pq = new PriorityQueue<>();
 
         for(int i = 0; i < N; i++) {
-            String[] xy = br.readLine().split(" ");
-            arr[i][0] = Integer.parseInt(xy[0]);
-            arr[i][1] = Integer.parseInt(xy[1]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            pq.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
 
-        Arrays.sort(arr, (e1, e2) -> {
-            return (e1[0] != e2[0]) ? e1[0] - e2[0] : e1[1] - e2[1];
-        });
-
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < N; i++) {
-            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        while(!pq.isEmpty()) {
+            Point p = pq.poll();
+            sb.append(p.x).append(" ").append(p.y).append("\n");
         }
 
         System.out.print(sb);
