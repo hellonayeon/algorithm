614e7516fd4ef7c841de2917719c7a20c653fdb6
hellonayeon <hellonykwon@gmail.com>
2021-12-22 18:10:33 +0900

    좌표 정렬하기 [코드수정]

--- BAEKJOON/Silver V/Q11650.java
+++ BAEKJOON/Silver V/Q11650.java
@@ -2,44 +2,30 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 class Q11650 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        List<Coordinate> coordinates = new ArrayList<>();
+        int[][] arr = new int[N][2];
 
-        while(N-- > 0) {
+        for(int i = 0; i < N; i++) {
             String[] xy = br.readLine().split(" ");
-            coordinates.add(new Coordinate(Integer.parseInt(xy[0]), Integer.parseInt(xy[1])));
+            arr[i][0] = Integer.parseInt(xy[0]);
+            arr[i][1] = Integer.parseInt(xy[1]);
         }
 
-        Collections.sort(coordinates);
+        Arrays.sort(arr, (e1, e2) -> {
+            return (e1[0] != e2[0]) ? e1[0] - e2[0] : e1[1] - e2[1];
+        });
 
         StringBuilder sb = new StringBuilder();
-        for(Coordinate cor : coordinates) {
-            sb.append(cor.x).append(" ").append(cor.y).append("\n");
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
         }
 
         System.out.print(sb);
-    }
-
-    static class Coordinate implements Comparable<Coordinate>{
-        int x;
-        int y;
-
-        public Coordinate(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public int compareTo(Coordinate o) {
-            return (this.x == o.x) ? this.y - o.y : this.x - o.x;
-        }
     }
 }
