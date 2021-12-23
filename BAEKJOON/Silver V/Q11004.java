467e6195512f6e5c40bce616501d4542e3e69918
hellonayeon <hellonykwon@gmail.com>
2021-12-23 20:14:52 +0900

    K번째 수 [알고리즘 수정] RE

--- BAEKJOON/Silver V/Q11004.java
+++ BAEKJOON/Silver V/Q11004.java
@@ -1,24 +1,48 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
 import java.util.StringTokenizer;
 
-class Q11004 {
+public class Q11004 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine() , " ");
+        int board[] = new int[N];
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MIN_VALUE;
 
-        int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        // left : 입력된 수들 중 가장 작은 값
+        // right: 입력된 수들 중 가장 큰 값
+        for(int i = 0; i < N; i++){
+            board[i] = Integer.parseInt(st.nextToken());
+            if(board[i] < left)
+                left = board[i];
+            if(board[i] > right)
+                right = board[i];
         }
 
-        Arrays.sort(arr);
+        // 탐색할 범위를 반씩 줄여나가며
+        // K번째로 큰 수 찾기 (mid)
+        int ans = 0;
+        while(left <= right){
+            int mid = (left+right)/2;
+            int count = 0;
+            for(int i = 0; i < N; i++){
+                if(board[i] <= mid)
+                    count++;
+            }
 
-        System.out.println(arr[K-1]);
+            // count가 K보다 크거나 같은 경우는
+            // K번째 수가 mid보다 작다는 의미이다.
+            if(count >= K){
+                right = mid-1;
+                ans = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        
+        System.out.println(ans);
     }
 }
