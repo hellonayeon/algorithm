13b39d4e915793c4a2f42a13aa8947639f153ba1
hellonayeon <hellonykwon@gmail.com>
2022-01-07 21:54:53 +0900

    큰 수의 법칙

--- 이것이 취업을 위한 코딩테스트다/그리디/Q2.java
+++ 이것이 취업을 위한 코딩테스트다/그리디/Q2.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Q3_2 {
+class Q2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // N, M, K
