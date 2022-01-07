019aaec37040b1ea0afa2ca8d786613812078ad4
hellonayeon <hellonykwon@gmail.com>
2022-01-07 21:55:55 +0900

    숫자 카드 게임

--- 이것이 취업을 위한 코딩테스트다/그리디/Q3.java
+++ 이것이 취업을 위한 코딩테스트다/그리디/Q3.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Q3_3 {
+class Q3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
