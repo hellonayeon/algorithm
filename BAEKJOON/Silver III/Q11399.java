fcde382d4dde3ff0510a4560e07e413bbe84c5cd
hellonayeon <hellonykwon@gmail.com>
2022-01-14 17:41:07 +0900

    ATM [알고리즘 수정]

--- BAEKJOON/Silver III/Q11399.java
+++ BAEKJOON/Silver III/Q11399.java
@@ -1,27 +1,30 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Q11399 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int N = Integer.parseInt(br.readLine());
+        /* int N = */ Integer.parseInt(br.readLine());
+        int[] cnt = new int[1001]; // 대기시간 빈도수
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i=0; i<N; i++) {
-            pq.add(Integer.parseInt(st.nextToken()));
+        int max = 0;
+        while(st.hasMoreTokens()) {
+            int idx = Integer.parseInt(st.nextToken());
+            cnt[idx]++;
+            max = Math.max(max, idx);
         }
 
         int acc = 0;
         int sum = 0;
-        while(!pq.isEmpty()) {
-            int p = pq.poll();
-            acc += p;
-            sum += acc;
+        for(int i=1; i<=max; i++) {
+            if(cnt[i] == 0) continue;
+            while(cnt[i]-- > 0) {
+                acc += i;
+                sum += acc;
+            }
         }
 
         System.out.println(sum);
