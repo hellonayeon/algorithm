823c9c4810d2263c046bc6246945c45554879891
hellonayeon <hellonykwon@gmail.com>
2022-04-14 15:16:20 +0900

    1952. 수영장 [DP]

--- SW Expert Academy/모의 SW 역량테스트/Q1952.java
+++ SW Expert Academy/모의 SW 역량테스트/Q1952.java
@@ -14,7 +14,7 @@ import java.io.FileInputStream;
 class Q1952 {
 
     static int[] fee = new int[4]; // 1일, 1달, 3달, 1년 요금
-    static int[] plan = new int[13];
+    static int[] dp = new int[13];
 
     static int res;
 
@@ -34,38 +34,24 @@ class Q1952 {
             // 1년 이용 계획 입력
             st = new StringTokenizer(br.readLine());
             for (int i=1; i<=12; i++) {
-                plan[i] = Integer.parseInt(st.nextToken());
+                // 이용일
+                int days = Integer.parseInt(st.nextToken());
+
+                // 1일 이용권과 1달 이용권 요금 비교
+                dp[i] = Math.min(dp[i-1] + days*fee[0], dp[i-1] + fee[1]);
+                
+                // 3달 이용권 요금 비교
+                // 3달 이전까지의 최소 요금 + 현재 구한 3달 요금
+                // 1, 2월인 경우 (i=1, i = 2) 이 분기는 고려하지 않아도 돼
+                // why? 문제의 조건에서 12, 1, 2월의 경우는 불가능하다 했으니까
+                if (i >= 3) {
+                    dp[i] = Math.min(dp[i], dp[i-3] + fee[2]);
+                }
             }
 
-            res = fee[3]; // 초기 최소 비용
-            dfs(1, 0);
-
-            sb.append("#").append(tc).append(" ").append(res).append("\n");
+            sb.append("#").append(tc).append(" ").append(Math.min(dp[12], fee[3])).append("\n");
         }
 
         System.out.println(sb);
-    }
-
-    private static void dfs(int month, int sum) {
-        if (sum >= res) return;
-
-        if (month > 12) {
-            res = Math.min(res, sum);
-            return;
-        }
-
-        if (plan[month] == 0) {
-            dfs(month + 1, sum);
-        }
-        else {
-            // 1일 이용 요금
-            dfs(month + 1, sum + fee[0]*plan[month]);
-
-            // 1달 이용 요금
-            dfs(month + 1, sum + fee[1]);
-            
-            // 3달 이용 요금
-            dfs(month + 3, sum + fee[2]);
-        }
     }
 }
