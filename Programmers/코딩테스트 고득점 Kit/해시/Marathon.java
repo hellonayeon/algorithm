2fc649a9fa6a351fc5ec81b15b2a7562be8ee7e3
hellonayeon <hellonykwon@gmail.com>
2022-04-19 17:29:43 +0900

    완주하지 못한 선수 [다시 풀이]

--- Programmers/코딩테스트 고득점 Kit/해시/Marathon.java
+++ Programmers/코딩테스트 고득점 Kit/해시/Marathon.java
@@ -3,20 +3,22 @@ import java.util.Map;
 
 class Marathon {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-
         Map<String, Integer> map = new HashMap<>();
-
-        for(String p : participant) map.put(p, map.getOrDefault(p, 0) + 1);
-        for(String c : completion) map.put(c, map.get(c) - 1);
-
-        for(String key : map.keySet()) {
-            if(map.get(key) != 0) { 
-                answer = key;
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        for (String c : completion) {
+            map.put(c, map.get(c)-1);
+        }
+        
+        String ans = "";
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                ans = key;
                 break;
             }
         }
-
-        return answer;
+        
+        return ans;
     }
 }
