65058c0582f8c68c8d85e7a7ad0c6dfcb081c9ef
hellonayeon <hellonykwon@gmail.com>
2022-04-21 23:33:33 +0900

    전화번호 목록 [다시 풀이]

--- Programmers/코딩테스트 고득점 Kit/해시/PhoneNumber.java
+++ Programmers/코딩테스트 고득점 Kit/해시/PhoneNumber.java
@@ -1,13 +1,17 @@
 import java.util.Arrays;
 
-class PhoneNumber {
-    public static boolean solution(String[] phone_book) {
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
         Arrays.sort(phone_book);
-
-        for(int i = 0; i < phone_book.length - 1; i++) {
-            String prefix = phone_book[i];
-            if(phone_book[i+1].startsWith(prefix)) return false;
+        for (int i=0; i<phone_book.length-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
         }
-        return true;
+        
+        return answer;
     }
 }
