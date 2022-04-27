f18478025a3372a6e083f969596daa45cee8cb06
hellonayeon <hellonykwon@gmail.com>
2022-04-27 23:43:08 +0900

    프린터 [다시 풀이]

--- Programmers/코딩테스트 고득점 Kit/스택 큐/Printer.java
+++ Programmers/코딩테스트 고득점 Kit/스택 큐/Printer.java
@@ -1,40 +1,60 @@
-class Printer {
-    public static int solution(int[] priorities, int location) {
-        int ans = 0;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Iterator;
 
-        for(int i = 0; i < priorities.length - 1; i++) {
-            // 뒤에 있는 문서들 중에 우선순위가 높은 문서가 있는지 탐색
-            int max_idx = i;
-            for(int j = i+1; j < priorities.length; j++) {
-                if(priorities[j] > priorities[max_idx]) max_idx = j;
-            }
-            // 이동이 필요한 경우
-            if(max_idx != i) {
-                priorities = move(priorities, ans, max_idx - i);
-
-                // 인쇄하고자 하는 문서의 위치값 변경
-                location -= max_idx - i;
-                if(location < ans) location -= ans;
-                if(location < 0) location += priorities.length; 
-            }
-
-            ans++;
-
-            if(i == location) return ans;
+class Solution {
+    
+    class Document {
+        int priority;
+        int location;
+        
+        public Document(int priority, int location) {
+            this.priority = priority;
+            this.location = location;
         }
-
-        // 마지막에 인쇄되는 경우
-        return ++ans;
     }
-
-    // 위치를 이동시킬 배열, 배열의 처음 위치, 이동시킬 칸 수
-    public static int[] move(int[] arr, int rear, int move_cnt) {
-        int[] tmp = new int[arr.length];
-        for(int i = rear + 1; i < arr.length; i++) {
-            int idx = (i + move_cnt >= arr.length) ? (i + move_cnt) % arr.length + rear : i + move_cnt;
-            tmp[i] = arr[idx];
+    
+    public int solution(int[] priorities, int location) {
+        Queue<Document> queue = new LinkedList<>();
+        
+        for (int i=0; i<priorities.length; i++) {
+            queue.add(new Document(priorities[i], i));
         }
-
-        return tmp;
+        
+        int order = 0;
+        while (!queue.isEmpty()) {
+            int locDiff = findHighestPriorityLocation(queue);
+            System.out.println(locDiff);
+            while (locDiff-- > 0) {
+                Document doc = queue.poll();
+                queue.add(doc);
+            }
+            
+            Document doc = queue.poll();
+            order++;
+            
+            if (doc.location == location) {
+                return order;
+            }
+        }
+        
+        return order;
+    }
+    
+    private int findHighestPriorityLocation(Queue<Document> queue) {
+        Iterator<Document> it = queue.iterator();
+        
+        Document firstDoc = it.next();
+        int highestPriority = firstDoc.priority;
+        int loc = 1, locDiff = 0;
+        while (it.hasNext()) {
+            Document doc = it.next();
+            if (doc.priority > highestPriority) {
+                highestPriority = doc.priority;
+                locDiff = loc;
+            }
+            loc++;
+        }
+        return locDiff;
     }
 }
