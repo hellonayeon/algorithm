05602ca44d00e7ad4df1e2124c315a8fb241cd30
hellonayeon <hellonykwon@gmail.com>
2022-04-28 21:34:14 +0900

    다리를 지나는 트럭 [다시 풀이 - 실패]

--- Programmers/코딩테스트 고득점 Kit/스택 큐/Truck.java
+++ Programmers/코딩테스트 고득점 Kit/스택 큐/Truck.java
@@ -1,50 +1,62 @@
-// 출처: https://www.pymoon.com/entry/Programmers-%EB%8B%A4%EB%A6%AC%EB%A5%BC-%EC%A7%80%EB%82%98%EB%8A%94-%ED%8A%B8%EB%9F%AD-Java
+import java.util.*;
 
-import java.util.LinkedList;
-import java.util.Queue;
+class Solution {
+    class Truck {
+        int weight;
+        int move;
 
-class Truck {
-    public static void main(String[] args) {
-        int bridge_length = 3;
-        int weight = 10;
-        int[] truck_weights = {7, 4, 3, 3, 6};
-        
-        System.out.println(solution(bridge_length, weight, truck_weights));
+        public Truck(int weight) {
+            this.weight = weight;
+            this.move = 1;
+        }
+
+        public void moving() {
+            move++;
+        }
     }
 
-    public static int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> bridge = new LinkedList<>(); // 다리를 지나고 있는 트럭들의 무게 큐
+    public int solution(int bridgeLength, int weight, int[] truckWeights) {
+        Queue<Truck> waitQ = new LinkedList<>();
+        Queue<Truck> moveQ = new LinkedList<>();
 
-        // 초기에 다리 위에는 첫 번째 트럭만 올라가 있다.
-        for(int i = 0; i < truck_weights.length - 1; i++)
-            bridge.add(0);
-
-        int idx = 1; 
-        int tw = truck_weights[idx]; // 다리에 올라가 있는 트럭들 무게의 합
-        int sec = 1; // 모든 트럭이 다리를 지나는 데 걸린 시간
-        
-        if(truck_weights.length == 1) return bridge_length + 1;
-
-        bridge.add(tw);
-        while(!bridge.isEmpty()) {
-            sec++; // while loop 실행되는 횟수 = 걸린 시간
-            tw -= bridge.poll(); // 트럭이 한 칸 이동
-
-            // 대기하는 트럭이 남아 있는 경우
-            if(idx < truck_weights.length) {
-                // 다음 트럭이 다리 위에 올라와도 무게를 초과하지 않는다면
-                // 다음 트럭을 다리 위로 올린다.
-                if(tw + truck_weights[idx] <= weight) {
-                    tw += truck_weights[idx];
-                    bridge.add(truck_weights[idx++]);
-                }
-                // 다음 트럭이 다리 위로 올라올 경우 무게를 초과한다면
-                // 현재 다리 위에 있는 트럭들만 앞으로 이동시킨다.
-                else {
-                    bridge.add(0);
-                }
-            }  
+        for (int t : truckWeights) {
+            waitQ.offer(new Truck(t));
         }
-        return sec;
+
+        int answer = 0;
+        int curWeight = 0;
+
+        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
+            answer++;
+
+            // 다리가 비어있다면 트럭 올리기
+            if (moveQ.isEmpty()) {
+                Truck t = waitQ.poll();
+                curWeight += t.weight;
+                moveQ.offer(t);
+                continue;
+            }
+
+            // 다리 위에 있는 트럭들을 한 칸씩 이동
+            for (Truck t : moveQ) {
+                t.moving();
+            }
+
+            // 가장 먼저 출발한 트럭의 이동 횟수가 다리 길이보다 길다면
+            // 다리에서 트럭 빼기
+            if (moveQ.peek().move > bridgeLength) {
+                Truck t = moveQ.poll();
+                curWeight -= t.weight;
+            }
+
+            // 대기하는 다음 트럭을 다리 위에 올릴 수 있는 경우
+            if (!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) {
+                Truck t = waitQ.poll();
+                curWeight += t.weight;
+                moveQ.offer(t);
+            }
+        }
+
+        return answer;
     }
 }
