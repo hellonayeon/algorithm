d217cc2fb6a41070017f93af486ff9f021c103b8
hellonayeon <hellonykwon@gmail.com>
2022-05-01 23:40:34 +0900

    주식가격 [시간초과]

--- Programmers/코딩테스트 고득점 Kit/스택 큐/StockPrice.java
+++ Programmers/코딩테스트 고득점 Kit/스택 큐/StockPrice.java
@@ -1,13 +1,45 @@
 class StockPrice {
-    public int[] solution(int[] prices) {
-        int[] ans = new int[prices.length];
+    class Stock {
+        int price;
+        int sec;
+        boolean stop;
         
-        for(int i = 0; i < prices.length - 1; i++) {
-            for(int j = i + 1; j < prices.length; j++) {
-                ans[i]++;
-                if(prices[j] < prices[i]) break;
+        public Stock(int price) {
+            this.price = price;
+            sec = 0;
+            stop = false;
+        }
+        
+        public void countSec(int curPrice) {
+            if (!stop) {
+                sec++;
+            }
+                
+            if (price > curPrice) {
+                stop = true;
             }
         }
-        return ans;
+    }
+    
+    
+    public int[] solution(int[] prices) {
+        Stock[] stocks = new Stock[prices.length];
+        for (int i=0; i<prices.length; i++) {
+            stocks[i] = new Stock(prices[i]);
+        }
+        
+        for (int i=1; i<prices.length; i++) {
+            int curPrice = prices[i];
+            for (int j=0; j<i; j++) {
+                stocks[j].countSec(curPrice);
+            }
+        }
+        
+        int[] answer = new int[stocks.length];
+        for (int i=0; i<stocks.length; i++) {
+            answer[i] = stocks[i].sec;
+        }
+        
+        return answer;
     }
 }
