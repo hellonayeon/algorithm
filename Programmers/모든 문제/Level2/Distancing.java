d135543a5737eb09a0b6b5d313e941d81352c654
hellonayeon <hellonykwon@gmail.com>
2022-05-03 15:28:02 +0900

    거리두기 확인하기 [다시 풀이]

--- Programmers/모든 문제/Level2/Distancing.java
+++ Programmers/모든 문제/Level2/Distancing.java
@@ -1,51 +1,61 @@
-// 맨해튼 거리가 2 이하인 경우만 확인
+import java.util.Arrays;
 
 class Distancing {
     
-    int[] dx = {0, -1, 0, 1};
-    int[] dy = {-1, 0, 1, 0};
+    int size = 5;
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+    int res;
     
     public int[] solution(String[][] places) {
-        int[] ans = new int[5];
+        int[] answer = new int[size];
+        Arrays.fill(answer, 1);
         
-        for(int i = 0; i < 5; i++) ans[i] = 1;
-        
-        for(int p = 0; p < 5; p++) {
-            
-            boolean distancing = true;
-            boolean[][] visit = new boolean[5][5];
-            
-            for(int y = 0; y < 5; y++) {
-                for(int x = 0; x < 5; x++) {
-                    if(places[p][y].charAt(x) == 'P' && !check(places[p], visit, x, y, 0)) {
-                        distancing = false;
-                        break;
-                    }       
-                }
-                if(!distancing) { 
-                    ans[p] = 0;
-                    break;
+        for (int room = 0; room < size; room++) {
+            res = 1;
+            for (int x = 0; x < size; x++) {
+                String row = places[room][x];
+                for (int y = 0; y < size; y++) {
+                    char c = row.charAt(y);
+                    
+                    if (c == 'P') {
+                        boolean[][] visit = new boolean[size][size];
+                        visit[x][y] = true;
+                        checkDistance(places[room], x, y, 0, visit, false);
+                        answer[room] = res;
+                    }
                 }
             }
         }
         
-        return ans;
+        
+        return answer;
     }
     
-    public boolean check(String[] place, boolean[][] visit, int x, int y, int dis) {
-        if(dis == 2) return true;
-        visit[y][x] = true;
-        
-        for(int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-            
-            if(nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
-            if(!visit[ny][nx] && place[ny].charAt(nx) != 'X') { 
-                if(place[ny].charAt(nx) == 'P' || !check(place, visit, nx, ny, dis + 1)) return false;
+    private void checkDistance(String[] place, int cx, int cy, int dis, boolean[][] visit, boolean partition) {
+        if (dis != 0 && place[cx].charAt(cy) == 'P') {
+            if (dis > 2 || (dis <= 2 && partition)) {
+                return;
             }
+            res = 0;
+            return;
         }
         
-        return true;
+        if (place[cx].charAt(cy) == 'X') {
+            partition = true;
+        }
+        
+        for (int k=0; k<4; k++) {
+            int nx = cx + dx[k];
+            int ny = cy + dy[k];
+            
+            if (nx < 0 || ny < 0 || nx >= size || ny >= size || visit[nx][ny]) {
+                continue;
+            }
+            
+            visit[nx][ny] = true;
+            checkDistance(place, nx, ny, dis + 1, visit, partition);
+            visit[nx][ny] = false;
+        }
     }
 }
